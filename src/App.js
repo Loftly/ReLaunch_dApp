d055c3b638434181644d7ff6565c10da995a3a6d
G G <gag1621@jagmail.southalabama.edu>
2023-10-15 09:40:45 -0500

    wrapped app.js w/ global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// App.js
 import React from 'react';
-import './styles/StyledGlobal.js';
+import GlobalStyle from './styles/StyledGlobal';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -11,18 +10,21 @@ import BettingPage from './pages/BettingPage';
 
 function App() {
     return (
-        <Router>
-            <Navbar />
-            <div className="app-content">
-                <Switch>
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/lottery" component={LotteryPage} />
-                    <Route path="/marsmissions" component={MarsMissionsPage} />
-                    <Route path="/betting" component={BettingPage} />
-                </Switch>
-            </div>
-            <Footer />
-        </Router>
+        <>
+            <GlobalStyle />
+            <Router>
+                <Navbar />
+                <div className="app-content">
+                    <Switch>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/lottery" component={LotteryPage} />
+                        <Route path="/marsmissions" component={MarsMissionsPage} />
+                        <Route path="/betting" component={BettingPage} />
+                    </Switch>
+                </div>
+                <Footer />
+            </Router>
+        </>
     );
 }
 
